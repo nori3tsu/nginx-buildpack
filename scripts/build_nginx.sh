b5fc09dab7b9eb1ecb97d4114825f7309dfea68a
山下　徳光 <worst1_best1@yahoo.co.jp>
2013-11-26 19:48:23 +0900

    modify build script

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #
-# Run with:
+# Required:
 # $ heroku config:set AWS_S3_BUCKET=public-bucket
-# $ heroku run 'curl https://gist.github.com/ejholmes/7120501/raw/build_nginx.sh | sh'
+#
+# Options:
+# $ heroku config:set NGINX_VERSION=1.5.2
+# $ heroku config:set PCRE_VERSION=8.21
+# $ heroku config:set NGINX_CONFIGURE_OPTIONS="--with-http_ssl_module --with-http_gzip_static_module --with-pcre=pcre-${PCRE_VERSION} --prefix=$HOME"
+#
+# Run with:
+# $ heroku run 'curl http://${AWS_S3_BUCKET}/build_nginx.sh | sh'
 
-NGINX_VERSION=1.5.2
-PCRE_VERSION=8.21
+NGINX_VERSION=${NGINX_VERSION-1.5.2}
+PCRE_VERSION=${PCRE_VERSION-8.21}
+CONFIGURE_OPTIONS=${CONFIGURE_OPTIONS-"--with-http_ssl_module --with-http_gzip_static_module --with-pcre=pcre-${PCRE_VERSION} --prefix=$HOME"}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
@@ -18,7 +26,7 @@ cd nginx-${NGINX_VERSION}
 echo "Downloading $pcre_tarball_url"
 curl $pcre_tarball_url | tar xjf -
 
-./configure --with-http_ssl_module --with-http_gzip_static_module --with-pcre=pcre-${PCRE_VERSION} --prefix=$HOME && make install
+./configure ${CONFIGURE_OPTIONS} && make install
 
 cd $HOME/sbin
 
